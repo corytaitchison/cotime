3143c005416bf5f2bf3fdaa8460d3f47963da092
Cory Aitchison <cory.aitchison@sydney.edu.au>
2024-08-17 16:06:12 +0100

    Initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,40 @@
 <script>
-  import { get_random } from './utils.js';
+  const subtractdate = (date, x) => { 
+    var newdate = new Date(date);
+    newdate.setTime(newdate.getTime() - x*1000);
+    return newdate
+  }
+  const daysInThisMonth = () => {
+    var now = new Date();
+    return new Date(now.getFullYear(), now.getMonth()+1, 0).getDate();
+  }
+  const printtime = (date) => date.toLocaleTimeString("en-US");
 
-  const boats = [
-    "boat",
-    "yacht",
-    "sailboat",
-    "dinghy",
-    "tug boat",
-    "cruise liner",
-    "🚢",
-    "motor boat",
-    "catamaran",
-    "kayak",
-    "ship",
-    "speed boat",
-    "fishing boat",
-    "pirate ship",
-    "submarine",
-    "ferry",
-    "houseboat",
-    "skiff",
-    "canoe",
-    "barge",
-    "navy cruiser",
-    "gondola",
-    "lifeboat",
-    "water taxi",
-    "big boat",
-    "small boat",
-    "cool boat",
-    "fast boat",
-    "slow boat"
-  ];
+  let monthday;
+  let monthtotal;
+  const offset = () => {
+    monthday = now.getDate();
+    monthtotal = daysInThisMonth(); 
+    var monthfrac = monthday / monthtotal; 
+    return monthfrac * 60 * 60;
+  }
 
-  let boat = get_random(boats);
+  let now;
+  const updateTime = () => {
+    now = new Date(); 
+  }
+  updateTime();
+  setInterval(updateTime, 1000);
 
-  const update_boat = () => {
-    let temp = boat;
-    while (temp === boat) {
-      temp = get_random(boats);
-    };
-    boat = temp;
-  };
-
-  let exclamations = "";
 </script>
 
-
 <main>
-  <h1 class="noselect">i'm a {boat} <strong>•</strong> help{exclamations}</h1>
-
-  <br>
-
-  <div id="ugbuttons" class="noselect">
-    <button on:click={update_boat}> Chug </button>
-    <button on:click={() => {exclamations = exclamations + "!"}}> Tug </button>
-  </div>
+  <h1 class="noselect"> Cotime is <strong>{printtime(subtractdate(now,
+    offset()))}</strong>. </h1>
+  <h2 class="noselect"> (Day {monthday} out of {monthtotal}) </h2>
+  <h2 class="noselect"> Actual time is {printtime(now)}. You are {Math.round(offset()
+  / 60 * 100)/100} minutes behind.</h2>
 </main>
 
 <style>
-  #ugbuttons > button {
-   margin: 0em 2em; 
-  }
 </style>
